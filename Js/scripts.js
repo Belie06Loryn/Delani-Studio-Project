bda85280006488927c168734eac8e1aaf78c4e35
Belie06Loryn <maniralexie@gmail.com>
2019-07-26 13:46:45 +0200

    adding JQuery for hide and show

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,1 +1,32 @@
-java
+$(".img1").click(function(){
+    $("#p1").toggle();
+    $(".img1").hide();
+    $("#p1").show();
+    });
+$(".img2").click(function(){
+    $("#p2").toggle();
+    $(".img2").hide();
+    $("#p2").show();
+  });
+$(".img3").click(function(){
+    $("#p3").toggle();
+    $(".img3").hide();
+    $("#p3").show();
+  });
+  $("#p1").click(function(){
+    $(".img1").toggle();
+    $("#p1").hide();
+    $(".img1").show();
+  });
+$("#p2").click(function(){
+    $(".img2").toggle();
+    $("#p2").hide();
+    $(".img2").show();
+  });
+$("#p3").click(function(){
+    $(".img3").toggle();
+    $("#p3").hide();
+    $(".img3").show();
+  });
+
+  
