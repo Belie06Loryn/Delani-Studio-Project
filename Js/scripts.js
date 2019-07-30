a1f7b01e8c67169900dfa3afa262541a2f310899
Belie06Loryn <maniralexie@gmail.com>
2019-07-30 08:57:58 +0200

    designing hover look

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
-$(".img1").click(function(){
+  $(".img1").click(function(){
     $("#p1").toggle();
     $(".img1").hide();
     $("#p1").show();
-    });
-$(".img2").click(function(){
+  });
+  $(".img2").click(function(){
     $("#p2").toggle();
     $(".img2").hide();
     $("#p2").show();
   });
-$(".img3").click(function(){
+  $(".img3").click(function(){
     $("#p3").toggle();
     $(".img3").hide();
     $("#p3").show();
@@ -19,35 +19,28 @@ $(".img3").click(function(){
     $("#p1").hide();
     $(".img1").show();
   });
-$("#p2").click(function(){
+  $("#p2").click(function(){
     $(".img2").toggle();
     $("#p2").hide();
     $(".img2").show();
   });
-$("#p3").click(function(){
+  $("#p3").click(function(){
     $(".img3").toggle();
     $("#p3").hide();
     $(".img3").show();
   });
 });
 
-  $(document).ready(function(){
-    $("#mybutton").click(function(messag){
-        var name = document.getElementById("name").value;
-        var messaje = document.getElementById("mybutton").value;{
-           alert(name +' '+ "We Have Recieved Your Message!");
-        }
-    });
-});
-
-
 $(document).ready(function(){
-    $(".image").click(function(){
-        $(".imaje").toggle();
-        $(".image").hide();
-        $(".imaje").show();
-      });
+  $("#mybutton").click(function(messag){
+    var name = document.getElementById("name").value;
+    var messaje = document.getElementById("mybutton").value;{
+      alert(name +' '+ "We Have Recieved Your Message!");
+    }
+  });
 });
+
+
 
 
 
